e3f4dafae8ba03fb62b93c08c00270a5a00f0ea2
umer <m.umer4058umer@gmail.com>
2026-06-12 00:14:46 +0500

    Modified date removed from sitemap.ts , email address updated in footer

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,12 +4,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://hybrixai.com',
-      lastModified: new Date(),
+      // lastModified: new Date(),
       priority: 1,
     },
     {
       url: 'https://hybrixai.com/contact',
-      lastModified: new Date(),
+      // lastModified: new Date(),
       priority: 0.8,
     },
   ]
